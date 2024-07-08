1e7146982b74e5fc5ec3d48afdfe06f4e9815e50
arunk <zen.code.symphony@gmail.com>
2024-07-08 11:54:45 +0530

    feat: added ID for api call tracking.

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,11 @@
+import crypto from 'crypto';
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+export const idForTracking = crypto
+  .createHash('sha256')
+  .update('openaiplayground')
+  .digest('hex');
--- src/openai-test.ts
+++ src/openai-test.ts
@@ -1,9 +1,11 @@
-import { openai } from './init';
+import { idForTracking, openai } from './init';
 
 async function main() {
+  console.log(`id for tracking: ${idForTracking}`);
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
     model: 'gpt-3.5-turbo',
+    user: idForTracking,
   });
 
   console.log(completion.choices[0]);
